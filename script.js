2f2a6601577c38f3f4505bcdaad8070f1d6db82e
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-03 16:41:38 +0700

    project finished

--- script.js
+++ script.js
@@ -15,6 +15,17 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const message = [
+  'No Number!!',
+  'Correct Number!',
+  'You Lost the game!',
+  'Start guessing...',
+];
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -22,11 +33,13 @@ document.querySelector('.check').addEventListener('click', function () {
   // input no number
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!!';
+    // displayMessage('mmeadsd');
 
     // When player win
   } else if (guess === secretNumber) {
     document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.message').textContent = 'Correct Number!';
+    // document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage(message[1]);
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -34,37 +47,56 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
+  } else if (guess !== secretNumber) {
+    console.log(score);
+    score--;
+    if (score > 0) {
+      // document.querySelector('.message').textContent =
+      // guess > secretNumber ? 'Too High' : 'Too Low';
 
-    // when the number too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High';
-      score--;
+      displayMessage(guess > secretNumber ? 'Too High' : 'Too Low');
+
       document.querySelector('.score').textContent = score;
-    } else {
+    } else if (score <= 0) {
       document.querySelector('.message').textContent = 'You Lost the game!';
+
       document.querySelector('.score').value = 0;
-    }
-
-    // when the number too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Low';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You Lost the game!';
-      document.querySelector('.score').textContent = 0;
+      // displayMessage(message[2]);
     }
   }
 });
+
+// when the number too high
+//   } else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = 'Too High';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'You Lost the game!';
+//       document.querySelector('.score').value = 0;
+//     }
+
+//     // when the number too low
+//   } else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = 'Too Low';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'You Lost the game!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+// });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage(message[3]);
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '?';
